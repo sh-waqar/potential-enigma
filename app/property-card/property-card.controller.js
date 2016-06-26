633e5ff44b9fb84d630b26d6b65041368c246250
Waqar Ali <waqar.ali911@gmail.com>
2016-06-26 22:34:48 +0400

    Production script to get a runable application.

--- app/property-card/property-card.controller.js
+++
@@ -1,8 +0,0 @@
-class PropertyCardController {
-  constructor() {
-  	// Check the state from binding and decide the label for button
-    this.actionLabel = this.state === 'result' ? 'Add Property' : 'Remove Property';
-  }
-}
-
-export default PropertyCardController;
