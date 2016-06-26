7f8f665bfb0fc008a490509e971507105fc1f1ea
Waqar Ali <waqar.ali911@gmail.com>
2016-06-26 12:25:48 +0400

    Added build script for production code with cache busting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-var webpack = require('webpack');
-var path = require('path');
-var autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+const config = {
   context: path.resolve(__dirname, 'app'),
   entry: {
     bundle: './app.js',
     vendors: ['angular']
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'app'),
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     loaders: [{
@@ -26,9 +27,19 @@ module.exports = {
     }]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
   ],
   postcss: function() {
     return [autoprefixer];
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.output.path = __dirname + '/dist';
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = config;
